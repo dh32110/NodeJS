c366c6fdf68c4bacdf790b41b09b2f471d675095
dh32110 <dh32110@naver.com>
2023-04-24 21:44:54 +0900

    node

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { type } from "os";
 
 const app = express();
 
@@ -17,15 +18,17 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser✅");
   socket.on("close", () => {
     "Disconnected to Browser❌";
   });
   socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
   });
-  socket.send("hello!");
 });
 
 server.listen(3000, handleListen);
