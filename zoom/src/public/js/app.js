09966fe01a79761626c7e245f905f6b462b114e7
dh32110 <dh32110@naver.com>
2023-04-26 18:17:30 +0900

    대화방 입장과 대화기능 구현

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -2,14 +2,54 @@ const socket = io();
 
 const welcome = document.querySelector("#welcome");
 const form = welcome.querySelector("form");
+const room = document.querySelector("#room");
 
-function handleRoomSubmit(e) {
+let roomName;
+
+room.hidden = true;
+
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleSubmitMessage(e) {
   e.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("server is done!");
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
   });
   input.value = "";
 }
 
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleSubmitMessage);
+}
+
+function handleRoomSubmit(e) {
+  e.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+}
+
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("Someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("Someone left");
+});
+
+socket.on("new_message", addMessage);
